d5da4db1352a3aececcdce5715db9900c9a68028
Iseki <isekiyui@atanasoff.cs.grinnell.edu>
2024-12-13 16:50:10 -0600

    testing

--- poissonblending.m
+++ poissonblending.m
@@ -4,97 +4,150 @@
 close all; clear; clc;
 
 
-%% Setting Up
+%% Setting Up Dataset
 
-% importing images and converting to double
+% importing images and matrices from dataset
+
+% obj:
+%   4-D array of object images, colored
+%       1: raft
+%       2: dog
+%       3: cowboy
+%       4: bird
+%       5: deer
+%       6: monkey
+%       7: camera cam
+load('mat/objects.mat')             % loads obj
+
+% bg:
+%   4-D array of background images, colored
+%       1: fall road
+%       2: hill
+%       3: mountain
+%       4: ocean
+load('mat/backgrounds.mat')         % loads bg
+
+% obj_N: 
+%   a matrix of number of neighboring pixels(N <= 4) within object image
+%   boundary (i.e. neighbor matrix of object image)
+load('mat/objects_N.mat')           % loads obj_N
+
+% obj_logical:
+%   a matrix of object image that dictates whether the corresponding pixel 
+%   is within object image boundary (i.e. logical matrix of object image)
+load('mat/objects_logical.mat')     % loads obj_logical
+
+
  
-load('mat/objects.mat')
-load('mat/backgrounds.mat')
+%% Simple Copy-and-Paste (Milestone 1)
 
-% a matrix of number of neighboring pixels in object image (N<=4 for boundary pixel)
-load('mat/objects_N.mat')
-load('mat/objects_logical.mat')
-
+% initializing copy-and-paste demo
 copy_paste = zeros(400, 600, 3, 28);
 
-for i= 1:4
-    for j = 1:7
-        background = bg(:,:,:,i);
-        object = obj(:,:,:,j);
+% number of background images
+num_bg = size(bg, 4);
 
-        [new_background, new_object] = imagepaste(background, object);
+% number of object images
+num_obj = size(obj, 4);
+
+% looping through each background / object image combination to demonstrate
+% copy-and-paste result
+for i= 1:num_bg
+    for j = 1:num_obj
+
+        % using imagepaste function to paste object image to background
+        % image
+        [new_background, new_object] = imagepaste(bg(:,:,:,i), obj(:,:,:,j));
+        
+        % saving result 
         copy_paste(:,:,:,i*7+j) = new_background + new_object;
     end
 end
 
- montage(copy_paste);
+% showing all combination of copy-and-paste results in montage 
+montage(copy_paste);
 
-%% Poisson equation
 
-A = bg(:,:,:,4);
-B = obj(:,:,:,1);
-B_log = obj_logical(:,:,1);
-H = zeros(size(B));
-N = obj_N(:, :, 2);
+%% Poisson Image Editing (Milestone 2-3)
 
-B_mask = zeros(size(B));
-B_mask(:, :, 1) = B_log;
-B_mask(:, :, 2) = B_log;
-B_mask(:, :, 3) = B_log;
+% TO-DO: test for
+%       color images
+%       grayscale images
+%       object gradient (seamless cloning)
+%       mixed gradient
+%   test for all combinations of color and guidance vector
 
-B_mask = rgb2gray(B_mask);
+% -------- testing/saving images for: 1. color / object gradient ---------
 
-[r, c] = find(B_mask == 1);
-r_max = max(r);
+% setting up data for PoissonSolver
+%   *later on in milestone 4, these processes have been made into a
+%   function for easier + more versatile image blending
+
+A = bg(:,:,:,4);                % background image
+B = obj(:,:,:,1);               % object image
+B_log = obj_logical(:,:,1);     % logical matrix of raft 
+N = obj_N(:, :, 2);             % neighbor matrix of raft
+
+% cropping the B_log matrix
+[r, c] = find(B_log == 1);
+r_max = max(r);         % getting highest/lowest location of object image boundary
 r_min = min(r);
 c_max = max(c);
 c_min = min(c);
 
-max_h = r_max - r_min;
+max_h = r_max - r_min;  % getting height/width of object image boundary
 max_w = c_max - c_min;
 
-B = imcrop(B, [c_min r_min max_w max_h]);
+B = imcrop(B, [c_min r_min max_w max_h]);   % cropping the object image
 
-figure;
-imshow(B);
+% figure;
+% imshow(B);
+% title('cropped object image B');
+imwrite(B, 'testing/cropped-B-obj.jpg')     % saving cropped object image
 
-B_mask = imcrop(B_mask, [c_min r_min max_w max_h]);
+B_log = imcrop(B_log, [c_min r_min max_w max_h]);
 
-B_mask(1, :) = 0;
-B_mask(end, :) = 0;
-B_mask(:, 1) = 0;
-B_mask(:, end) = 0;
-se = strel('disk', 5);
-B_mask = imerode(B_mask, se);
+% boundary condition
+B_log(1, :) = 0;        % making outer border of mask to match background image
+B_log(end, :) = 0;
+B_log(:, 1) = 0;
+B_log(:, end) = 0;  
+se = strel('disk', 5);  % eroding mask
+B_log = imerode(B_log, se);
 
-figure;
-imshow(B_mask);
-title('the mask, OBJ_MASK')
+% figure;
+% imshow(B_log);
+% title('the mask, OBJ_MASK');
+imwrite(B_log, 'testing/cropped-B-mask.jpg')     % saving cropped object image mask (logical matrix)
 
 % cutting out background image
 TRG(:,:,1) = A(r_min:r_max, c_min:c_max, 1);
 TRG(:,:,2) = A(r_min:r_max, c_min:c_max, 2);
 TRG(:,:,3) = A(r_min:r_max, c_min:c_max, 3);
 
-figure;
-imshow(TRG);
-title('background image, IMG_BG')
+% figure;
+% imshow(TRG);
+% title('background image, IMG_BG')
+imwrite(TRG, 'testing/cropped-A-bg.jpg')     % saving cropped background
 
-figure;
-imshow(B);
-title('original object image, IMG_OBJ');
-
-im_out = PoissonSolver(TRG,B,B_mask,0,0);
+% calling PoissonSolver function to adjust object image
+img_final = PoissonSolver(TRG,B,B_log,0,0);
 
 % pasting new object image
-A(r_min:r_max,c_min:c_max,1) = im_out(:,:,1);
-A(r_min:r_max,c_min:c_max, 2) = im_out(:,:,2);
-A(r_min:r_max,c_min:c_max, 3) = im_out(:,:,3);
+A(r_min:r_max,c_min:c_max,1) = img_final(:,:,1);
+A(r_min:r_max,c_min:c_max, 2) = img_final(:,:,2);
+A(r_min:r_max,c_min:c_max, 3) = img_final(:,:,3);
 
-figure;
-imshow(im_out, []);
-title('final object image');
+% figure;
+% imshow(img_final, []);
+% title('final object image');
+imwrite(img_final, 'testing/final-B-obj.jpg')     % saving final object image
 
-figure;
-imshow(A, []);
-title('final image');
+% figure;
+% imshow(A, []);
+% title('final image');
+imwrite(A, 'testing/final-img.jpg')     % saving final image
+
+%% Versatile and automatic Poisson Image Editing! (Milestone 4)
+
+
