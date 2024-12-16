41aab6368d1588dcdf8675e6c3f46fb780b101ad
Shuta Shibue <shibuesh@grinnell.edu>
2024-12-15 22:41:32 -0600

    added comments made create dataset into function

--- create_colored_dataset.m
+++ create_colored_dataset.m
@@ -1,65 +1,66 @@
+function create_colored_dataset()
+    % Image locations for objects and backgrounds
+    base_path_obj = 'source_images/colored/objects/';
+    base_path_bg = 'source_images/colored/backgrounds/';
 
-% Image locations for objects and backgrounds
-base_path_obj = 'source_images/colored/objects/';
-base_path_bg = 'source_images/colored/backgrounds/';
-
-% load background images and save into matrix
-% image path: source_images/colored/backgrounds/background_name.jpg
-% destination path: mat/colored/backgrounds/background_name.mat
-% format: bg, 400x600x3 double
-bg_names = ["fall_road", "grass", "mountains", "ocean"];
-for i= 1:4
-    bg = im2double(imread(strcat(base_path_bg, bg_names(i), ".jpg")));
-    save(strcat("mat/colored/backgrounds/", bg_names(i), ".mat"), 'bg');
-end
-
-
-obj_names = ["raft", "dog", "cowboy", "bird", "deer", "monkey", "person"];
-% load background images and save into matrix
-% path: source_images/colored/objects/object_name.jpg
-% format:   obj, 400x600x3 double
-%           logical_mask, 400x600 logical
-%           N, 400x600 double
-for img_i = 1:7
-    obj = im2double(imread(strcat(base_path_obj, obj_names(img_i), ".jpg")));
-
-
-    % Set non-object part to NaN. For each pixels from left, turn the pixels to 0
-    % until we find a pixel such that not(all(r,g,b>0.95)).
-    for i = 1:size(obj, 1)
-        for j = 1:size(obj, 2)
-            if all(obj(i, j, :) > 0.95)
-                obj(i, j, :) = NaN;
-            else
-                break;
-            end
-        end
+    % load background images and save into matrix
+    % image path: source_images/colored/backgrounds/background_name.jpg
+    % destination path: mat/colored/backgrounds/background_name.mat
+    % format: bg, 400x600x3 double
+    bg_names = ["fall_road", "grass", "mountains", "ocean"];
+    for i= 1:4
+        bg = im2double(imread(strcat(base_path_bg, bg_names(i), ".jpg")));
+        save(strcat("mat/colored/backgrounds/", bg_names(i), ".mat"), 'bg');
     end
 
-    % do it from the right side of image too. Note that this is not the
-    % perfect trimming algorithm, but we can remove most of white
-    % backgrounds while keeping white elements in the actual object.
-    for i = 1:size(obj, 1)
-        for j = size(obj, 2):-1:1
-            if all(obj(i, j, :) > 0.95)
-                obj(i, j, :) = NaN;
-            else
-                break;
+
+    obj_names = ["raft", "dog", "cowboy", "bird", "deer", "monkey", "person"];
+    % load background images and save into matrix
+    % path: source_images/colored/objects/object_name.jpg
+    % format:   obj, 400x600x3 double
+    %           logical_mask, 400x600 logical
+    %           N, 400x600 double
+    for img_i = 1:7
+        obj = im2double(imread(strcat(base_path_obj, obj_names(img_i), ".jpg")));
+
+
+        % Set non-object part to NaN. For each pixels from left, turn the pixels to 0
+        % until we find a pixel such that not(all(r,g,b>0.95)).
+        for i = 1:size(obj, 1)
+            for j = 1:size(obj, 2)
+                if all(obj(i, j, :) > 0.95)
+                    obj(i, j, :) = NaN;
+                else
+                    break;
+                end
             end
         end
+
+        % do it from the right side of image too. Note that this is not the
+        % perfect trimming algorithm, but we can remove most of white
+        % backgrounds while keeping white elements in the actual object.
+        for i = 1:size(obj, 1)
+            for j = size(obj, 2):-1:1
+                if all(obj(i, j, :) > 0.95)
+                    obj(i, j, :) = NaN;
+                else
+                    break;
+                end
+            end
+        end
+
+        % make it to logical matrix with true for object and false for non-object
+        logical_mask = true(size(obj,1), size(obj,2));
+        logical_mask(isnan(obj(:,:,1))) = false;
+
+        % The value of N matrix shows how many of adjacent pixels are objects.
+        % So the value is between 0(no adjecency with object) to 4 (completely
+        % inside the object).
+        % convolution to get N matrix
+        kernel = [0 1 0; 1 0 1; 0 1 0];
+        N = conv2(logical_mask, kernel, 'same');
+
+        % save the object, N, and logical_mask
+        save(strcat("mat/colored/objects/", obj_names(img_i), ".mat"), 'obj', 'N', 'logical_mask');
     end
-
-    % make it to logical matrix with true for object and false for non-object
-    logical_mask = true(size(obj,1), size(obj,2));
-    logical_mask(isnan(obj(:,:,1))) = false;
-
-    % The value of N matrix shows how many of adjacent pixels are objects.
-    % So the value is between 0(no adjecency with object) to 4 (completely
-    % inside the object).
-    % convolution to get N matrix
-    kernel = [0 1 0; 1 0 1; 0 1 0];
-    N = conv2(logical_mask, kernel, 'same');
-
-    % save the object, N, and logical_mask
-    save(strcat("mat/colored/objects/", obj_names(img_i), ".mat"), 'obj', 'N', 'logical_mask');
 end
--- create_transparent_dataset.m
+++ create_transparent_dataset.m
@@ -1,58 +1,59 @@
+function create_transparent_dataset()
+    % Image locations for objects and backgrounds
+    base_path_obj = 'source_images/transparent/objects/';
+    base_path_bg = 'source_images/transparent/backgrounds/';
 
-% Image locations for objects and backgrounds
-base_path_obj = 'source_images/transparent/objects/';
-base_path_bg = 'source_images/transparent/backgrounds/';
-
-% load background images and save into matrix
-% image path: source_images/transparent/backgrounds/background_name.png
-% destination path: mat/transparent/backgrounds/background_name.mat
-% format: bg, 400x600x3 double
-bg_names = ["old_paper", "raspberry_books", "ruled_paper"];
-for i= 1:3
-    bg = im2double(imread(strcat(base_path_bg, bg_names(i), ".png")));
-    save(strcat("mat/transparent/backgrounds/", bg_names(i), ".mat"), 'bg');
-end
+    % load background images and save into matrix
+    % image path: source_images/transparent/backgrounds/background_name.png
+    % destination path: mat/transparent/backgrounds/background_name.mat
+    % format: bg, 400x600x3 double
+    bg_names = ["old_paper", "raspberry_books", "ruled_paper"];
+    for i= 1:3
+        bg = im2double(imread(strcat(base_path_bg, bg_names(i), ".png")));
+        save(strcat("mat/transparent/backgrounds/", bg_names(i), ".mat"), 'bg');
+    end
 
 
-% load all images and save into matrix
-% image path: source_images/transparent/objects/object_name.png
-% destination path: mat/transparent/objects/object_name.mat
-% format: I, 400x600x3 double
-%         alpha, 400x600 double
-%         composite, 400x600x3 double
-%         N, 400x600 double
-%         logical_mask, 400x600 logical
-%         composite_nan, 400x600x3 double
-obj_names = ["bunny", "sibley", "wings", "zodiac"];
-for i= 1:4
+    % load all images and save into matrix
+    % image path: source_images/transparent/objects/object_name.png
+    % destination path: mat/transparent/objects/object_name.mat
+    % format: I, 400x600x3 double
+    %         alpha, 400x600 double
+    %         composite, 400x600x3 double
+    %         N, 400x600 double
+    %         logical_mask, 400x600 logical
+    %         composite_nan, 400x600x3 double
+    obj_names = ["bunny", "sibley", "wings", "zodiac"];
+    for i= 1:4
 
-    % load image and alpha
-    [I,~,alpha] = imread(strcat(base_path_obj, obj_names(i), ".png"));
-    I = im2double(I);
-    alpha = im2double(alpha);
+        % load image and alpha
+        [I,~,alpha] = imread(strcat(base_path_obj, obj_names(i), ".png"));
+        I = im2double(I);
+        alpha = im2double(alpha);
 
-    % create background image with alpha channel
-    background = ones(size(I));
-    alpha_3 = repmat(alpha, [1 1 3]);
+        % create background image with alpha channel
+        background = ones(size(I));
+        alpha_3 = repmat(alpha, [1 1 3]);
 
-    % output = image * alpha + background * (1 - alpha)
-    composite = I .* alpha_3 + background .* (1 - alpha_3);
+        % output = image * alpha + background * (1 - alpha)
+        composite = I .* alpha_3 + background .* (1 - alpha_3);
 
-    % set non-object part to NaN
-    composite_nan = composite;
-    composite_nan(composite(:,:,1) > 0.95) = NaN;
+        % set non-object part to NaN
+        composite_nan = composite;
+        composite_nan(composite(:,:,1) > 0.95) = NaN;
 
-    % create logical mask
-    logical_mask = true(size(composite_nan,1), size(composite_nan,2));
-    logical_mask(isnan(composite_nan(:,:,1))) = false;
-    
-    % The value of N matrix shows how many of adjacent pixels are objects.
-    % So the value is between 0(no adjecency with object) to 4 (completely
-    % inside the object).
-    % convolution to get N matrix
-    kernel = [0 1 0; 1 0 1; 0 1 0];
-    N = conv2(logical_mask, kernel, 'same');
+        % create logical mask
+        logical_mask = true(size(composite_nan,1), size(composite_nan,2));
+        logical_mask(isnan(composite_nan(:,:,1))) = false;
+        
+        % The value of N matrix shows how many of adjacent pixels are objects.
+        % So the value is between 0(no adjecency with object) to 4 (completely
+        % inside the object).
+        % convolution to get N matrix
+        kernel = [0 1 0; 1 0 1; 0 1 0];
+        N = conv2(logical_mask, kernel, 'same');
 
-    % save all variables
-    save(strcat("mat/transparent/objects/", obj_names(i), ".mat"), 'I', 'alpha', 'composite', 'N', 'logical_mask', 'composite_nan');
+        % save all variables
+        save(strcat("mat/transparent/objects/", obj_names(i), ".mat"), 'I', 'alpha', 'composite', 'N', 'logical_mask', 'composite_nan');
+    end
 end
