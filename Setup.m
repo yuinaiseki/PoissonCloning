2739534cd074a401ea319199760866972e0c0d85
Shuta Shibue <shibuesh@grinnell.edu>
2024-12-15 23:04:17 -0600

    added comments

--- Setup.m
+++ Setup.m
@@ -1,7 +1,7 @@
 % Create folders for storing images and matrices
 
 if not(exist('mat','dir'))
-    mkdir('mat');
+    mkdir('mat'); % all processed image data will be stored here
     mkdir('mat/colored');
     mkdir('mat/colored/backgrounds');
     mkdir('mat/colored/objects');
@@ -11,7 +11,7 @@ if not(exist('mat','dir'))
 end
 
 if not(exist('testing','dir'))
-    mkdir('testing');
+    mkdir('testing'); % store output images here
 end
 
 % create .mat dataset
