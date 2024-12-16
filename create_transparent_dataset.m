84b0bb3ef67b7647494fa5d25287e5ff5d0655f7
Shuta Shibue <shibuesh@grinnell.edu>
2024-12-15 21:22:15 -0600

    Added transparent data

--- create_transparent_dataset.m
+++ create_transparent_dataset.m
@@ -3,81 +3,42 @@
 base_path_obj = 'transparent_source/objects/';
 base_path_bg = 'transparent_source/backgrounds/';
 
-% set up matrix - 4 images for objects and 3 images for backgrounds
-obj = zeros(400, 600, 3, 4);
-alpha = zeros(400, 600, 4);
-bg =  zeros(400, 600, 3, 3);
+bg_names = ["old_paper", "raspberry_books", "ruled_paper"];
+for i= 1:3
+    bg = im2double(imread(strcat(base_path_bg, bg_names(i), ".png")));
+    save(strcat("mat/transparent/backgrounds/", bg_names(i), ".mat"), 'bg');
+end
+
 
 % load all images and save into matrix
 obj_names = ["bunny", "sibley", "wings", "zodiac"];
 for i= 1:4
-    [inpict,~,alpha] = imread(strcat(base_path_obj, obj_names(i), ".png"));
-    obj(:,:,:,i) = im2double(inpict);
-    alpha(:,:,i) = im2double(alpha);
-end
+    [I,~,alpha] = imread(strcat(base_path_obj, obj_names(i), ".png"));
+    I = im2double(I);
+    alpha = im2double(alpha);
+
+    background = ones(size(I));
+    alpha_3 = repmat(alpha, [1 1 3]);
+
+    % output = image * alpha + background * (1 - alpha)
+    composite = I .* alpha_3 + background .* (1 - alpha_3);
 
 
-bg_names = ["old_paper", "raspberry_books", "ruled_paper"];
-for i= 1:3
-    bg(:,:,:,i) = im2double(imread(strcat(base_path_bg, bg_names(i), ".png")));
-end
-
-% use montage to show all image in the dataset
-figure;
-montage(bg, 'Size', [2 2]);
-figure;
-montage(obj, 'Size', [2 2]);
-figure;
-montage(alpha, 'Size', [2 2]);
+    composite_nan = composite;
+    composite_nan(composite(:,:,1) > 0.95) = NaN;
 
 
-% Set non-object part to NaN. For each pixels from left, turn the pixels to 0
-% until we find a pixel such that not(all(r,g,b>0.95)).
-obj_logical = zeros(400, 600, 4);
-obj_N = zeros(400, 600, 4);
-for img = 1:4
-    object = obj(:,:,:,img);
-    for i = 1:size(object, 1)
-        for j = 1:size(object, 2)
-            if all(object(i, j, :) > 0.95)
-                object(i, j, :) = NaN;
-            else
-                break;
-            end
-        end
-    end
 
-    % do it from the right side of image too. Note that this is not the
-    % perfect trimming algorithm, but we can remove most of white
-    % backgrounds while keeping white elements in the actual object.
-    for i = 1:size(object, 1)
-        for j = size(object, 2):-1:1
-            if all(object(i, j, :) > 0.95)
-                object(i, j, :) = NaN;
-            else
-                break;
-            end
-        end
-    end
-
-    obj(:,:,:,img) = object;
-    logical_mask = true(size(object,1), size(object,2));
-    logical_mask(isnan(object(:,:,1))) = false;
+    logical_mask = true(size(composite_nan,1), size(composite_nan,2));
+    logical_mask(isnan(composite_nan(:,:,1))) = false;
     % make it to logical matrix
-    obj_logical(:,:,img) = logical_mask;
-
+    
     % The value of N matrix shows how many of adjacent pixels are objects.
     % So the value is between 0(no adjecency with object) to 4 (completely
     % inside the object).
     % convolution to get N matrix
     kernel = [0 1 0; 1 0 1; 0 1 0];
-    N = conv2(obj_logical(:,:,img), kernel, 'same');
-    obj_N(:,:,img) = N;
-end
+    N = conv2(logical_mask, kernel, 'same');
 
-% saving matrices as files
-save('mat/transparent/objects.mat', 'obj');
-save('mat/transparent/backgrounds.mat', 'bg');
-save('mat/transparent/objects_logical.mat', 'obj_logical');
-save('mat/transparent/objects_N.mat', 'obj_N');
-save('mat/transparent/alpha.mat', 'alpha');
+    save(strcat("mat/transparent/objects/", obj_names(i), ".mat"), 'I', 'alpha', 'composite', 'N', 'logical_mask', 'composite_nan');
+end
